7e8864019f3faf956b6fbc63136cf8c6dbf9a6e6
ElisaLissandra <elisa_lissandra@outlook.com>
2023-11-23 21:17:08 -0300

    Estrutura inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home'
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
+
 function App() {
   return (
-    <div className="App">
-        <p>Costs</p>
-    </div>
+   <Router>
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <Routes>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route exact path="/company">
+          <Company/>
+        </Route>
+        <Route exact path="/contact">
+          <Contact/>
+        </Route>
+        <Route exact path="/newproject">
+          <NewProject/>
+        </Route>
+      </Routes>
+      <p>Footer</p>
+   </Router>
   );
 }
 
