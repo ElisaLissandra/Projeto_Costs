73ca8131a44b7d0aa382eb08cc287f049172b5dd
ElisaLissandra <elisa_lissandra@outlook.com>
2023-11-29 21:56:15 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
   import React from 'react';
-  import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+  import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
   import Home from './components/pages/Home'
   import Contact from './components/pages/Contact'
   import Company from './components/pages/Company'
   import NewProject from './components/pages/NewProject'
-  import Container from './components/layout/Container';
+  import Projects from './components/pages/Projects'
+
+
+  import Container from './components/layout/Container'
+  import Navbar from './components/layout/Navbar'
+  import Footer from './components/layout/Footer'
+  
 
   function App() {
     return (
       <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo projeto</Link>
-      </div>
+        <Navbar />
         <Container customClass="min-height">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
             <Route path="/company" element={<Company />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/newproject" element={<NewProject />} />
           </Routes>
         </Container>
-      <p>Rodapé</p>
+      <Footer />
     </Router>
     );
   }
