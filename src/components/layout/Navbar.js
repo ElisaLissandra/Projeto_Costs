a8b24db2d8a42cd70add7292b3e2e9f9adfd7c05
ElisaLissandra <elisa_lissandra@outlook.com>
2023-11-30 21:08:45 -0300

    Footer

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -9,22 +9,22 @@ import logo from  '../../img/costs_logo.png'
 
 function Navbar() {
     return(
-        <nav class={styles.navbar}>
+        <nav className={styles.navbar}>
             <Container>
                 <Link to="/">
                     <img src={logo} alt="Costs" />
                 </Link>
-               <ul class={styles.list}>
-                    <li class={styles.item}>
+               <ul className={styles.list}>
+                    <li className={styles.item}>
                         <Link to="/">Home</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li className={styles.item}>
                         <Link to="/projects">Projetos</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li className={styles.item}>
                         <Link to="/company">Empresa</Link>
                     </li>
-                    <li class={styles.item}>
+                    <li className={styles.item}>
                         <Link to="/contact">Contato</Link>
                     </li>
                </ul>
