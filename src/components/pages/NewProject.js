2681c16f5236234894b284e2fc0cb6970e611a74
ElisaLissandra <elisa_lissandra@outlook.com>
2024-01-08 21:24:20 -0300

    Estrutura inicial do formalario

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,5 +1,14 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+
 function NewProject() {
-    return <h1>NewProject</h1>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar seu serviço</p>
+            <ProjectForm />
+        </div>
+    )
 }
 
 export default NewProject
