1b3b45dfbf55456f117cc1f181fb26beaeae09aa
ElisaLissandra <elisa_lissandra@outlook.com>
2024-01-09 21:57:32 -0300

    Componentização dos inputs do formulário

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,12 +1,22 @@
-    function ProjectForm() {
-        return (
-        <form>
-            <div>
-                <input type="text" placeholder="Insira o nome do projeto" />
-            </div>
-            <div>
-                <input type="number" placeholder="Insira o orçamento total" />
-            </div>
+import Input from '../form/Input'
+
+import styles from './ProjectForm.module.css' 
+
+function ProjectForm() {
+    return (
+        <form className={styles.form}>
+            <Input 
+            type="text" 
+            text="Nome do projeto"
+            name="name"
+            placeholder="Insira o nome do projeto"
+            />
+            <Input 
+            type="number" 
+            text="Orçamento do projeto"
+            name="budget"
+            placeholder="Insira o orçamento total"
+            />
             <div>
                 <select name="category_id">
                     <option disabled selected>Selecione a categoria</option>
@@ -16,8 +26,8 @@
                 <input type="submit" value="Criar projeto"/>
             </div>
         </form>
-        )
-    }
+    )
+}
 
 
     export default ProjectForm
