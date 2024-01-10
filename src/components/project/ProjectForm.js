d6e096499a579436e66b4f6fb8e7a347d5552624
ElisaLissandra <elisa_lissandra@outlook.com>
2024-01-10 20:48:27 -0300

    Componentização do select

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,4 +1,5 @@
 import Input from '../form/Input'
+import Select from '../form/Select'
 
 import styles from './ProjectForm.module.css' 
 
@@ -17,11 +18,7 @@ function ProjectForm() {
             name="budget"
             placeholder="Insira o orçamento total"
             />
-            <div>
-                <select name="category_id">
-                    <option disabled selected>Selecione a categoria</option>
-                </select>
-            </div>
+            <Select name="category_id" text="Selecione a categoria"/>
             <div>
                 <input type="submit" value="Criar projeto"/>
             </div>
