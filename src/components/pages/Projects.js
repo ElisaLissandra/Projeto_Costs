e5175d0b11f7c9141f1219d03160ff94097b9bb5
ElisaLissandra <elisa_lissandra@outlook.com>
2024-01-25 20:55:31 -0300

    Criação dinâmica da mensagem

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,10 +1,21 @@
+import { useLocation } from 'react-router-dom'
+
 import Message from '../layout/Message'
 
 function Projects() {
+
+    const location = useLocation()
+
+    let message = ''
+
+    if(location.state) {
+        message = location.state.message
+    }
+
     return (
         <div>
             <h1>Meus Projetos</h1>
-            <Message msg="Alguma mensagem" type="error"/>
+            {message && <Message type="success" msg={message} />} 
         </div>
     )
 }
