c48494b0aa2ef316812ad32a768f227624301b2b
ElisaLissandra <elisa_lissandra@outlook.com>
2024-04-08 18:08:39 -0300

    Implementação da função excluir

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -16,7 +16,8 @@ function Projects() {
 
     // Armazena as informações dos projetos
     const [projects, setProjects] = useState([])
-    const [removeLoading, setRemoveLoading] = useState(false)   
+    const [removeLoading, setRemoveLoading] = useState(false) 
+    const [projectMessage, setProjectMessage] = useState('');
 
     // Mensagem
     const location = useLocation()
@@ -41,6 +42,21 @@ function Projects() {
             .catch((err) => console.log(err))
     }, [])
 
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(resp => resp.json())
+        .then(() => {
+            setProjects(projects.filter((project) => project.id !== id))
+            setProjectMessage('Projeto removido com sucesso!')
+        })
+        .catch(err => console.log(err))
+    }
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
@@ -48,19 +64,22 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar projeto" />
             </div>
             {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => (
                         <ProjectCard
+                            id={project.id}
                             name={project.name}
                             budget={project.budget}
                             category={project.category ? project.category.name : 'Sem categoria'}
                             key={project.id}
+                            handleRemove={removeProject}
                             project={project} 
                         />
                     ))}
                     {!removeLoading && <Loading />}
-                    {removeLoading && projects.length == 0 && (
+                    {removeLoading && projects.length === 0 && (
                         <p>Não há projetos cadastrados!</p>
                     )}
             </Container>
