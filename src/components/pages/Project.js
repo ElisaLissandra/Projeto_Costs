1f172cd37b2ff132486b20a33b863215ccb1ab1d
ElisaLissandra <elisa_lissandra@outlook.com>
2024-05-09 10:29:33 -0300

    Remoção dos serviços

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -113,8 +113,31 @@ function Project() {
         .catch((err) => console.log(err))
     }
 
-    function removeService() {
+    function removeService(id, cost) {
+       
+        const servicesUpdated = project.services.filter(
+            (service) => service.id !== id,
+        )
+        
+        const projectUpdated = project
 
+        projectUpdated.services = servicesUpdated
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`,{
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(projectUpdated)
+        })
+        .then ((resp) => resp.json())
+        .then((data) => {
+            setProject(projectUpdated)
+            setServices(servicesUpdated)
+            setMessage('Serviço removido com sucesso!')
+        })
+        .catch(err => console.log(err))
     }
 
     // Exibe o formulário de edição
